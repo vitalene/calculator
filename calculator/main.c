36216e679d421524f61c525ba159110e793947b2
Neil <vitalene@msu.edu>
2016-08-08 13:36:37 -0400

    Initial Commit

---
+++ calculator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  calculator
+//
+//  Created by Neil Vitale on 8/8/16.
+//  Copyright © 2016 Neil Vitale. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
